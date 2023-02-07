db65666092aba8fd56df378fa9269f71a6efa311
Shubs <shubs@sqp.me>
2023-02-07 16:00:44 -0500

    working.

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { React, useState } from 'react';
 
-function GuessInput() {
-  return <div />;
-}
+const GuessInput = () => {
+  const [guessInput, setGuessInput] = useState('');
+
+  const handleChange = (event) => {
+    const guessInputUpperCase = event.target.value.toUpperCase();
+    setGuessInput(guessInputUpperCase);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ guessInput });
+    if (guessInput.length !== 5) {
+      window.alert('Please enter exactly 5 characters.');
+      return;
+    }
+    setGuessInput('');
+  };
+
+  return (
+    <form className='guess-input-wrapper' onSubmit={handleSubmit}>
+      <label htmlFor='guess-input'>Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        type='text'
+        id='guess-input'
+        name='guess-input'
+        value={guessInput}
+        onChange={(event) => handleChange(event)}
+      />
+    </form>
+  );
+};
 
 export default GuessInput;
