21b0a1ebac32f7b4f8bd33e43d0e13a0bfd3fafa
Shubs <shubs@sqp.me>
2023-02-07 16:16:54 -0500

    sucessful static guess list render.

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
 
-function GuessResults() {
-  return <div />;
-}
+const GuessResults = () => {
+  const guessList = [
+    { id: 1, label: 'FIRST' },
+    { id: 2, label: 'SECOND' },
+  ];
+
+  return (
+    <>
+      <div class='guess-results'>
+        {guessList.map(({ id, label }) => (
+          <p id={id} class='guess'>
+            {label}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default GuessResults;
