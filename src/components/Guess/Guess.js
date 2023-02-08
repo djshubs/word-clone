c4061a1f6d091dfa92ebd267ce4cc0d68013ea9a
Shubs <shubs@sqp.me>
2023-02-07 19:35:46 -0500

    pre-soln e4-GameLogic working

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { range } from '../../utils';
 import { WORD_LENGTH } from '../../constants';
+import { checkGuess } from '../../game-helpers';
+import { answer } from '../Game/Game';
 
 const Guess = ({ word }) => {
-  console.log({ word });
+  const wordArray = checkGuess(word, answer);
+
   return (
     <>
       <p className='guess'>
-        {range(WORD_LENGTH).map((num) => (
-          <span key={num} className='cell'>
-            {word ? word[num] : undefined}
-          </span>
-        ))}
+        {range(WORD_LENGTH).map((num) => {
+          const { letter, status } = wordArray ? wordArray[num] : {};
+          return (
+            <span key={num} className={`cell ${status}`}>
+              {letter ? letter : undefined}
+            </span>
+          );
+        })}
       </p>
     </>
   );
